e2e51e5f753bff442b8fcef73655f89fe0f8fe21
Nadide Demirbaş <nadidedemirbas@gmail.com>
2023-06-22 17:54:55 +0300

    Add tests title and poster

--- test/TableCell.test.js
+++ test/TableCell.test.js
@@ -5,12 +5,25 @@ import TableCell from '../src/components/TableCell.vue'
 
 describe('TableCell Component', () => {
     const wrapper = mount(TableCell, {
-        image: "mockimage",
-        title: "mock-title",
-        path: "mockpath"
+        props: {
+            image: "mockimage",
+            title: "mock-title",
+            path: "mockpath"
+        }
     })
 
     it('should render correctly', () => {
         expect(wrapper.html()).toMatchSnapshot();
+    })
+
+    it('should show title correctly', () => {
+        const title = wrapper.find('.index-movie-title-wrapper').text()
+        expect(title).toBe('mock-title');
+    })
+
+    it('should show poster correctly', () => {
+        const attributes = wrapper.find('.image-fluid').attributes();
+        const source = attributes.src;
+        expect(source).toBe('mockimage');
     })
 });
